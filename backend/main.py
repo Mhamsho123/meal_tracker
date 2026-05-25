203458bb0acf85a9b24dc7a7fd659b0779af6623
Mohamad Hamsho <hamsho1030@gmail.com>
2026-05-25 12:24:31 -0400

    Update meal tracker

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5501"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+meals = []
+
+@app.get("/")
+def home():
+    return {"message": "working"}
+
+@app.get("/meals")
+def get_meals():
+    return meals
+
+@app.post("/meals")
+def add_meal(meal: dict):
+    meals.append(meal)
+    return {"message": "meal added", "meals": meals}
---
+++ frontend/index.js
@@ -0,0 +1,26 @@
+const addBtn = document.getElementById('add-btn');
+
+addBtn.addEventListener('click', function(){
+
+    const userInput = document.getElementById('meal-input').value.trim()
+
+    if(!userInput){
+        console.log("Please enter a meal")
+        return
+    }
+
+    fetch("http://127.0.0.1:8000/meals", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: userInput
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+
+})
